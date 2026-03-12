16bf063b7a9e4f69ae82399f0ddee2c8eb6a19ee
Victory <victoryajayi90@gmail.com>
2026-03-12 13:54:16 +0100

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const ejs = require('ejs')
 const app = express();
 const port = 2123;
+
+app.set("view engine", "ejs")
+
+const name = "Emmanuel Gabriel";
 const students = [
     {
         Name: "John Doe",
@@ -68,10 +73,27 @@ const students = [
 
 app.get('/home', (req, res) => {
     res.send('Welcome Home')
+    console.log(__dirname);
+    
 })
 
 app.get("/student", (req, res) => {
     res.send(students)
+})
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html")
+
+})
+
+// app.get("/about", (req, res) => {
+//     res.render("index", {name: name})
+    
+// })
+
+
+app.get("/about", (req, res) => {
+    res.render("about", {name: name})
 })
 
 
